66c80ae0be86bda9989271bed866a5c1f3583818
KishanthSelvaraj <154578200+KishanthSelvaraj@users.noreply.github.com>
2024-07-13 11:06:34 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const port = process.env.PORT || 5000;
 const io = require("socket.io")(port, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: "https://jamboard-navy.vercel.app/",
     methods: ["GET", "POST"],
   },
 });
